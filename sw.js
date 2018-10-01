f533b0280e5b06039e0d6c4fa43ccbfde73fc94a
Ryan Woo <woo@Ryans-MacBook-Pro.local>
2018-10-01 02:24:27 -0700

    add functions of Install & Activate Service Worker

--- sw.js
+++ sw.js
@@ -9,10 +9,28 @@ let urlsToCache = [
   'data/restaurants.json',
 ];
 
+// Service Worker before get installed, get name, urls and addthem to cache
 self.addEventListener('install', function (event) {
   event.waitUntil(
     caches.open(staticCacheName).then( function (cache) {
       return cache.addAll(urlsToCache);
+    })
+  )
+});
+
+//activate the service Worker
+self.addEventListener('activate', function (event) {
+  console.log("New service getting activated...");
+  event.waitUntil(
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function (cacheName) {
+          return cacheName.startsWith('restaurant-') &&
+                 cacheName != staticCacheName;
+        }).map(function (cacheName) {
+          return caches.delete(cacheName)
+        })
+      );
     })
   )
 });
